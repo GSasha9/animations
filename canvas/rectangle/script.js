92c99f9574cf7d7d75cb27916a9b35a5f213d461
Alexandra Gurbanova <sasha.hurbnva@gmail.com>
2026-07-23 18:24:39 +0300

    canvas: add materials

---
+++ canvas/rectangle/script.js
@@ -0,0 +1,9 @@
+function drawRect() {
+    const canvas = document.getElementById("rect");
+    const ctx = canvas.getContext("2d");
+    ctx.fillRect(25, 25, 100, 100);
+    ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(50, 50, 50, 50);
+}
+
+drawRect()
